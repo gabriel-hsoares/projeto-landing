94343ff87fbad19a7033eee577c1410d3f3145e5
Gabriel Soares <116104655+gabriel-hsoares@users.noreply.github.com>
2022-12-12 00:09:23 -0300

    php

---
+++ envia_fale.php
@@ -0,0 +1,25 @@
+<?php
+$destinatario = "gabriel.beneficium@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$telefone = $_REQUEST['tel'];
+$interesse = $_REQUEST['interest'];
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Telefone: " . $tel . "\n\n";
+$body = $body . "Interesse: " . $interest . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:obrigado.htm");
+
